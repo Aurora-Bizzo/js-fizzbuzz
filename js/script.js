05a0ebe1cedc384fb8c23fb9f4c72f87911728c7
Aurora-Bizzo <aurora.bizzozero98@gmail.com>
2022-11-29 17:27:12 +0100

    exerise completed

--- js/script.js
+++ js/script.js
@@ -3,14 +3,14 @@ const container = document.querySelector('.box-area')
 for (let i = 1; i <= 100; i++) {
     let element;
 
-    if (i % 3 === 0) {
+    if (i % 5 === 0 && i % 3 === 0) {
+        element = `<div class="box box-red">FizzBuzz</div>`
+    }
+    else if (i % 3 === 0) {
         element = `<div class="box box-green">Fizz</div>`
     }
     else if (i % 5 === 0) {
         element = `<div class="box box-yellow">Buzz</div>`
-    }
-    else if (i % 5 === 0 && i % 3 === 0) {
-        element = `<div class="box box-red">FizzBuzz</div>`
     }
     else {
         element = `<div class="box box-blue">${i}</div>`

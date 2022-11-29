2d5157af2061921af0ec8548b850152332197b63
Aurora-Bizzo <aurora.bizzozero98@gmail.com>
2022-11-29 16:58:45 +0100

    fixed wew

--- js/script.js
+++ js/script.js
@@ -4,16 +4,16 @@ for (let i = 1; i <= 100; i++) {
     let element;
 
     if (i % 3 === 0) {
-        element = `<div class="box box-green>Fizz</div>"`
+        element = `<div class="box box-green">Fizz</div>`
     }
     else if (i % 5 === 0) {
-        element = `<div class="box box-yellow>Buzz</div>"`
+        element = `<div class="box box-yellow">Buzz</div>`
     }
     else if (i % 5 === 0 && i % 3 === 0) {
-        element = `<div class="box box-red>FizzBuzz</div>"`
+        element = `<div class="box box-red">FizzBuzz</div>`
     }
     else {
-        element = `<div class="box box-blue>${i}</div>"`
+        element = `<div class="box box-blue">${i}</div>`
     }
 
     container.innerHTML += element;
